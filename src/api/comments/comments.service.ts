1b3e6901a859e1a1aa234a7f5a19753266ee4bea
Yesid Hernandez <fyesid.h@gmail.com>
2024-02-04 08:50:23 -0500

    refactor: error handler added and implemented in comments module and refactorized comments

--- src/api/comments/comments.service.ts
+++ src/api/comments/comments.service.ts
@@ -1,8 +1,4 @@
-import {
-  ForbiddenException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
